a6bd6a69494b1ae66d45466360e084dbb62ed320
Andrew Pan <a@tny.town>
2021-12-08 00:52:29 -0600

    Revert "Merge branch 'mh-2021'"
    This reverts commit 483afa3151886350ad9a56a3c69beaa9b2fdb4df, reversing
    changes made to abf48555d90fe4a620f8ad2db93236bd44b98dbe.

--- static/signup.js
+++ static/signup.js
@@ -1,6 +1,6 @@
 let apiBase = "/api";
 
-$(document).ready(async () => {
+$(document).ready(function() {
 	let schools = new Bloodhound({
 		datumTokenizer: Bloodhound.tokenizers.whitespace,
 		queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -63,63 +63,19 @@ $(document).ready(async () => {
 
 	$("#minor").change(checkminor);
 
-	document.querySelector("#submit").disabled = false;
-    
-	// blame hemant
-    const form = document.querySelector("form");
-	const resumeInput = document.querySelector("input[name=resume]");
-    resumeInput.disabled = false;
-    
-    form.addEventListener("submit", async ev => {
-		ev.preventDefault();
-
-		// NB: disabling the input removes it from the all seeing eye of FormData.
-        resumeInput.disabled = true;
-        const data = new FormData(form);
-        resumeInput.disabled = false;
-
-        try {
-			let resId = await uploadResume(resumeInput.files[0]);
-			data.set("resume", resId);
-        } catch(e) {
-			console.warn("Invalid or nonexistent resume:", e);
-        }
-
-        console.log(`data: ${data}`);
-        try {
-			const url = await fetch(form.action, {
-				method: "POST",
-				mode: "same-origin",
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded"
-				},
-				body: new URLSearchParams(data).toString(),
-			}).then(r => r.redirected ? r.url : Promise.reject("Form submit failed: Expected redirect"));
-            
-            window.location = url;
-        } catch(e) {
-			submitErr("An error occurred whilst submitting your registration. Please try again.", e);
+	let checktransit = function() {
+		console.log($("#driving").prop('checked'), $("#bus").prop('checked'));
+		if ($("#driving").prop('checked') && $("#bus").prop('checked')){
+			$("#driving-bus-nag").show();
+			$("#submit").prop("disabled",true);
+		}else{
+			$("#driving-bus-nag").hide();
+			$("#submit").prop("disabled",false);
 		}
-	});
+	}
+
+	$("#driving").change(checktransit);
+	$("#bus").change(checktransit);
+	checktransit();
+
 });
-
-const submitErr = (msg, e) => {
-    console.error(e);
-	const box = document.querySelector("#submission-issue");
-    box.innerText = msg;
-};
-
-const uploadResume = resume => {
-    if(!resume) return Promise.reject("resume is falsy");
-	const data = new FormData();
-	data.append("resume", resume);
-
-    const hash = resume.arrayBuffer().then(ab => crypto.subtle.digest("SHA-256", ab));
-	return fetch(`${apiBase}/upload`, {
-		method: "POST",
-        mode: "same-origin",
-        body: data,
-    }).then(r => r.ok ? r : Promise.reject(`Resume submit failed: HTTP status ${r.status}`))
-		.then(() => hash.then(h => [...new Uint8Array(h)].map(b => b.toString(16).padStart(2, "0")).join("")))
-		.catch(e => submitErr("An error occured whilst uploading your resume. Please try again", e));
-};
